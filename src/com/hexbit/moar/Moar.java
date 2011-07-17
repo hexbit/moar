4a89cfe46fdfc1d6654c181df71e7f1766cd7263
hexbit <hexbit0x@gmail.com>
2011-07-17 02:29:13 -0400

    Cleaned up the moar functions and added basic functionality     modified:   src/com/hexbit/moar/Moar.java

--- src/com/hexbit/moar/Moar.java
+++ src/com/hexbit/moar/Moar.java
@@ -1,9 +1,9 @@
 package com.hexbit.moar;
 
 import java.util.logging.Logger;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
@@ -62,7 +62,7 @@ public class Moar extends JavaPlugin {
 			else if (args.length == 2) {
 				try {
 					int amount = Integer.parseInt(args[1]);
-					giveMoar(player, args[0], amount);
+					moar(player, args[0], amount);
 				}
 				catch (NumberFormatException e) {
 					player.sendMessage("Usage: /moar <block name> <#>");
@@ -73,18 +73,43 @@ public class Moar extends JavaPlugin {
 		}
 		
 		else if (commandLabel.equalsIgnoreCase("m")) {
-			ItemStack itemInHand = player.getInventory().getItemInHand();
-			itemInHand.setAmount(64);
+			moar(player);
+			return true;
 		}
 		
 		return false;
 	}
-	
-	private void giveMoar(Player player, String itemName, int ammount) {
-		//TODO: Give player the amount
+	//Polymorphism FTW
+	private void moar(Player player) {
+		if (player.getInventory().getItemInHand().getTypeId() != 0) {
+			ItemStack itemInHand = player.getInventory().getItemInHand();
+			itemInHand.setAmount(itemInHand.getMaxStackSize());
+		}
 	}
 	
+	//TODO: Check for max stack size
 	private void moar(Player player, int amount) {
-		//TODO: Give Player moar of what is in their hand
+		if (player.getInventory().getItemInHand().getTypeId() != 0) {
+			ItemStack itemInHand = player.getInventory().getItemInHand(); 
+			
+			if ((amount + itemInHand.getAmount()) > itemInHand.getMaxStackSize()) {
+				itemInHand.setAmount(itemInHand.getMaxStackSize());
+				ItemStack stack = itemInHand;
+				stack.setAmount(amount);
+				player.getWorld().dropItemNaturally(player.getLocation(), stack);
+			}
+			else {
+				itemInHand.setAmount(itemInHand.getAmount() + amount);
+			}
+		}
+	}
+	//Don't use this right now, item look up needs to be implemented first
+	private void moar(Player player, String itemName, int amount) {
+		//TODO: Look into if this is the best way to be doing this
+		MaterialData material = new MaterialData(1);
+		ItemStack stack = material.toItemStack();
+		//stack.setTypeId(typeId); 
+		//stack.setAmount(amount);
+		player.getWorld().dropItemNaturally(player.getLocation(), stack);
 	}
 }
